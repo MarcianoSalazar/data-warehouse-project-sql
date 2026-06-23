f8cddd0583dec92cb83346d78025914a7a2c2108
Marciano Salazar <168376936+MarcianoSalazar@users.noreply.github.com>
2026-06-23 23:22:58 +0800

    Update check_crm_cust_info.sql

--- scripts/silver/check_crm_cust_info.sql
+++ scripts/silver/check_crm_cust_info.sql
@@ -5,7 +5,7 @@ SELECT
 	COUNT(*)
 FROM silver.crm_cust_info
 GROUP BY cst_id
-HAVING COUNT(*) != 1 OR cst_id IS NULL
+HAVING COUNT(*) != 1 OR cst_id IS NULL;
 
 
 -- Check for Unwanted Spaces
@@ -13,14 +13,14 @@ HAVING COUNT(*) != 1 OR cst_id IS NULL
 SELECT 
 	cst_firstname
 FROM silver.crm_cust_info
-WHERE TRIM(cst_firstname) != cst_firstname
+WHERE TRIM(cst_firstname) != cst_firstname;
 
 
 
 -- Data Standardization & Consistency
 SELECT DISTINCT cst_gndr
-FROM silver.crm_cust_info
+FROM silver.crm_cust_info;
 
 -- Check overall data
 SELECT DISTINCT *
-FROM silver.crm_cust_info
+FROM silver.crm_cust_info;
