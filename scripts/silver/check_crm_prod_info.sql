46ccacdea36a5495ff222bdeb928aaa8b1fe43bb
Marciano Salazar <168376936+MarcianoSalazar@users.noreply.github.com>
2026-06-23 23:22:45 +0800

    Update check_crm_prod_info.sql

--- scripts/silver/check_crm_prod_info.sql
+++ scripts/silver/check_crm_prod_info.sql
@@ -5,14 +5,14 @@ SELECT
 	COUNT(*)
 FROM bronze.crm_prd_info
 GROUP BY prd_id
-HAVING COUNT(*) != 1 OR prd_id IS NULL
+HAVING COUNT(*) != 1 OR prd_id IS NULL;
 
 
 -- Check unwanted spaces 
 -- Expectation: No Result
 SELECT prd_nm
 FROM bronze.crm_prd_info
-WHERE prd_nm != TRIM(prd_nm)
+WHERE prd_nm != TRIM(prd_nm);
 
 	
 -- Check for Nulls Or Negative Nums
@@ -21,17 +21,17 @@ SELECT
 	prd_id,
 	prd_cost 
 FROM bronze.crm_prd_info
-WHERE prd_cost IS NULL OR prd_cost < 0
+WHERE prd_cost IS NULL OR prd_cost < 0;
 
 -- Data Standardization
-SELECT DISTINCT prd_line FROM bronze.crm_prd_info
+SELECT DISTINCT prd_line FROM bronze.crm_prd_info;
 
 
 -- Check for Invalid Date Orders
 -- Expectation: No Result
 SELECT *
 FROM bronze.crm_prd_info
-WHERE prd_start_dt > prd_end_dt	
+WHERE prd_start_dt > prd_end_dt;
 
 -- Check overall data
-SELECT * FROM silver.crm_prd_info
+SELECT * FROM silver.crm_prd_info;
