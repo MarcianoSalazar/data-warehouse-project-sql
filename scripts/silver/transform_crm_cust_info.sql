833f38f0c71d0ff8cca08a5293079dcf641018d2
Marciano Salazar <168376936+MarcianoSalazar@users.noreply.github.com>
2026-06-23 23:20:36 +0800

    Add files via upload

---
+++ scripts/silver/transform_crm_cust_info.sql
@@ -0,0 +1,57 @@
+TRUNCATE TABLE silver.crm_cust_info
+
+INSERT INTO silver.crm_cust_info (
+	cst_id,
+	cst_key,
+	cst_firstname,
+	cst_lastname,
+	cst_marital_status,
+	cst_gndr,
+	cst_create_date)
+SELECT
+	cst_id,
+	cst_key,
+	TRIM(cst_firstname) AS cst_firstname, -- Remove unwanted spaces in firstname
+	TRIM(cst_lastname) AS cst_lastname, -- Remove unwanted spaces in lastname
+	CASE WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Married'
+		 WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
+		 ELSE 'N/A' -- Handling NULL values
+	END cst_marital_status, -- Standardize marital status to readable format
+	CASE WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
+		 WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
+		 ELSE 'N/A' -- Handling NULL values
+	END cst_gndr,-- Standardize gender to readable format
+	cst_create_date
+FROM (
+	SELECT 
+	*,
+	ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) AS flag_last -- Remove the duplicates
+	FROM bronze.crm_cust_info
+	WHERE cst_id IS NOT NULL
+)t WHERE flag_last = 1 -- Select the most recent record per customer	
+
+
+
+
+
+
+
+
+
+/*
+WITH t AS (
+	SELECT 
+	*,
+	ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) AS flag_last
+	FROM bronze.crm_cust_info
+)
+SELECT 
+	cst_id,
+	cst_key,
+	TRIM(cst_firstname) AS cst_firstname,
+	TRIM(cst_lastname) AS cst_lastname,
+	cst_marital_status,
+	cst_gndr,
+	cst_create_date
+FROM t
+WHERE flag_last = 1 */
